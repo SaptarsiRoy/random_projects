c87c3f212145ed2852c4a82e03147f3c5236cdfb
SaptarsiRoy <saptarsiroy12@gmail.com>
2020-05-02 22:33:36 +0530

    guess game code

---
+++ guess_game.py
@@ -0,0 +1,25 @@
+'''
+simple guess game
+'''
+
+import random
+def play_game():
+    '''
+    function for playing the play_game
+    '''
+
+    guess = random.randint(1, 10)
+    chances = 3
+    while chances != 0:
+        usr_guess = input("Enter a number:")
+        if usr_guess == guess:
+            print("Congratulation!!! \nYou have won")
+            break
+        chances -= 1
+        print("Cances remaining:", chances)
+    if chances == 0:
+        print("Better luck next time :(")
+
+
+if __name__ == '__main__':
+    play_game()
