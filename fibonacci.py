459be4fccc52a84ffe397b0d5c0bae49f8d04ddc
SaptarsiRoy <saptarsiroy12@gmail.com>
2020-05-04 22:41:00 +0530

    nth fibonacci number

---
+++ fibonacci.py
@@ -0,0 +1,23 @@
+'''
+Program to find the nth Fibonacci number
+'''
+
+def Fibonacci(n):
+    '''
+    Function for nth Fibonacci number
+    '''
+	if n<0:
+		print("Incorrect input")
+	# First Fibonacci number is 0
+	elif n==1:
+		return 0
+	# Second Fibonacci number is 1
+	elif n==2:
+		return 1
+	else:
+		return Fibonacci(n-1)+Fibonacci(n-2)
+
+if __name__ == '__main__':
+    number = input("Enter a number:")
+    fibo = Fibonacci(number)
+    print("The fibonacci number is:", fibo)
