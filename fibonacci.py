92776341fc08f0bcfc4aa47cb1f1e2ee80fdf7e5
SaptarsiRoy <saptarsiroy12@gmail.com>
2020-05-04 22:49:12 +0530

    code for nth fibonacci number

--- fibonacci.py
+++ fibonacci.py
@@ -6,21 +6,21 @@ def Fibonacci(n):
     '''
     Function for nth Fibonacci number
     '''
-	if n<0:
-		print("Incorrect input")
-	# First Fibonacci number is 0
-	elif n==1:
-		return 0
-	# Second Fibonacci number is 1
-	elif n==2:
-		return 1
-	else:
-		return Fibonacci(n-1)+Fibonacci(n-2)
+    if n<0:
+        	print("Incorrect input")
+        # First Fibonacci number is 0
+    elif n==1:
+        	return 0
+        # Second Fibonacci number is 1
+    elif n==2:
+        	return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
 
 if __name__ == '__main__':
-    
+
     # input a number from user
-    number = input("Enter a number:")
+    number = int(input("Enter a number:"))
 
     #find the nth fibonacci number
     fibo = Fibonacci(number)
